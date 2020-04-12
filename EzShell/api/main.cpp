b0dee32cb4f18673eb9a9650f517ce5adcb8ee47
BakaSea <sea@infstudio.net>
2020-04-12 04:35:12 -0700

    fix some mistakes

--- EzShell/api/main.cpp
+++ EzShell/api/main.cpp
@@ -5,8 +5,8 @@ int main(int argc, char *argv[]) {
     if (argc < 3) return -1;
     DirHelper *dirHelper = new DirHelper();
     dirHelper->setPath(argv[2]);
-    CommandBase *command = NULL;;
+    CommandBase *command = NULL;
     //TODO: command = new CommandXXX(argv[1], dirHelper);
-    command->run();
+    if (command != NULL) command->run();
     return 0;
 }
